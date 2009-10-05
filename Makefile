483a668613487a39fba9fb4e74a968a04e0aca57
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-10-05 06:58:22 -0400

    initial commit of Pandora

---
+++ Makefile
@@ -0,0 +1,38 @@
+# Pandora master makefile
+# Copyright (C) 2009 Michael Richardson <mcr@sandelman.ca>
+# 
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+
+
+PANDORA_SRCDIR?=$(shell pwd)
+export PANDORA_SRCDIR
+
+TERMCAP=
+export TERMCAP
+
+include ${PANDORA_SRCDIR}/Makefile.inc
+
+srcdir?=$(shell pwd)
+
+include ${PANDORA_SRCDIR}/Makefile.top
+-include ${PANDORA_SRCDIR}/Makefile.vendor
+
+checkprograms::
+	@for d in $(SUBDIRS) ; \
+	do \
+		(cd $$d && $(MAKE) srcdir=${PANDORA_SRCDIR}/$$d/ PANDORA_SRCDIR=${PANDORA_SRCDIR} $@ ) || exit 1; \
+	done; 
+
+env:
+	@env | sed -e "s/'/'\\\\''/g" -e "s/\([^=]*\)=\(.*\)/\1='\2'/"
+
+
---
+++ testing/utils/verify-uml.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# show me
+#set -x
+
+# fail if any command fails
+set -e
+
+case $# in
+    1) PANDORA_SRCDIR=$1; shift;;
+esac
+
+if [ `id -u` = 0 ]
+then
+    echo Do not run this as root.
+    exit
+fi
+
+#
+# configuration for this file has moved to $PANDORA_SRCDIR/umlsetup.sh
+# By default, that file does not exist. A sample is at umlsetup-sample.sh
+# in this directory. Copy it to $PANDORA_SRCDIR and edit it.
+#
+if [ -z "${PANDORA_SRCDIR}" ] && [ -f umlsetup.sh ]
+then
+    PANDORA_SRCDIR=`pwd`
+fi
+
+PANDORA_SRCDIR=${PANDORA_SRCDIR-../..}
+if [ ! -f ${PANDORA_SRCDIR}/umlsetup.sh ]
+then
+    echo No umlsetup.sh. Please read instructions in doc/umltesting.html and testing/utils/umlsetup-sample.sh.
+    exit 1
+fi
+
+. ${PANDORA_SRCDIR}/umlsetup.sh
+
+if [ ! -d ${KERNPOOL}/. ]; then echo Your KERNPOOL= is not properly set; exit 1; fi	
+
+if [ "${UMLPATCH}" != "none" ] && [ ! -r ${UMLPATCH} ]; then echo Your UMLPATCH= is not properly set; exit 1; fi
+if [ -z "${PANDORA_HOSTS}" ]; then echo Your PANDORA_HOSTS= is not properly set; exit 1; fi
+if [ -z "${NATTPATCH}" ]; then echo Your NATTPATCH= is not properly set; exit 1; fi 
+if [ ! -d ${BASICROOT}/. ]; then echo Your BASICROOT= is not properly set; exit 1; fi
+    
