575aeb1231a7652f632c1bfdb0029f658ca78100
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-10-25 13:45:19 -0400

    build library with debugging decode ND packets that are received

--- include/iface.h
+++ include/iface.h
@@ -8,14 +8,25 @@ public:
 	int errors(void) {
 		return error_cnt;
 	}
+	void set_verbose(int flag) { verbose_flag = flag; }
+	void set_verbose(int flag, FILE *out) {
+		verbose_flag = flag;
+		verbose_file = out;
+	}
+	int  verboseprint() { return verbose_flag; }
 
 	virtual int send_packet(const u_char *bytes, const int len);
-	virtual void receive_packet(const u_char *bytes, const int len);
+	virtual void receive_packet(struct in6_addr ip6_src,
+				    struct in6_addr ip6_dst,
+				    const u_char *bytes, const int len);
 		
 
 private:
 	int nd_socket;
 	int error_cnt;
+	int verbose_flag;
+	FILE *verbose_file;
+#define VERBOSE(X) ((X)->verbose_flag && (X)->verbose_file!=NULL)
 };
 
 
--- lib/libndmgmt/iface.cpp
+++ lib/libndmgmt/iface.cpp
@@ -16,7 +16,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern "C" {
+#include <arpa/inet.h>
 #include <netinet/ip6.h>
+#include <netinet/icmp6.h>
+}
 
 #include "iface.h"
 
@@ -31,9 +35,83 @@ network_interface::network_interface(int fd)
     nd_socket = fd;
 }
 
-void network_interface::receive_packet(const u_char *bytes, const int len)
+void network_interface::receive_packet(struct in6_addr ip6_src,
+				       struct in6_addr ip6_dst,
+				       const u_char *bytes, const int len)
 {
-    return;
+    u_char *nd_options;
+    char src_addrbuf[INET6_ADDRSTRLEN];
+    char dst_addrbuf[INET6_ADDRSTRLEN];
+
+    /* should collect this all into a "class packet", or "class transaction" */
+    if(this->verbose_flag) {
+	inet_ntop(AF_INET6, &ip6_src, src_addrbuf, INET6_ADDRSTRLEN);
+	inet_ntop(AF_INET6, &ip6_dst, dst_addrbuf, INET6_ADDRSTRLEN);
+    }
+
+    struct icmp6_hdr *icmp6 = (struct icmp6_hdr *)bytes;
+    const u_char *bytes_end = bytes + len;
+    /* XXX should maybe check the checksum? */
+
+    switch(icmp6->icmp6_type) {
+    case ND_ROUTER_SOLICIT:
+    {
+	struct nd_router_solicit *nrs = (struct nd_router_solicit *)bytes;
+	nd_options = (u_char *)&nrs[1];
+	if(VERBOSE(this)) {
+	    fprintf(this->verbose_file, "Got router solicitation from %s with %u bytes of options\n",
+		    src_addrbuf,
+		    bytes_end - (u_char *)nd_options);
+	}
+    }
+    break;
+
+    case ND_ROUTER_ADVERT:
+    {
+	struct nd_router_advert *nra = (struct nd_router_advert *)bytes;
+	nd_options = (u_char *)&nra[1];
+	if(VERBOSE(this)) {
+	    fprintf(this->verbose_file, "Got router advertisement (reachable=%u, retransmit=%u) from %s with %u bytes of options\n",
+		    ntohl(nra->nd_ra_reachable),
+		    ntohl(nra->nd_ra_retransmit),
+		    src_addrbuf,
+		    bytes_end - (u_char *)nd_options);
+	}
+    }
+    break;
+
+    case ND_NEIGHBOR_SOLICIT:
+    {
+	struct nd_neighbor_solicit *nns = (struct nd_neighbor_solicit *)bytes;
+	nd_options = (u_char *)&nns[1];
+	if(VERBOSE(this)) {
+	    char target_addrbuf[INET6_ADDRSTRLEN];
+	    inet_ntop(AF_INET6, &nns->nd_ns_target, target_addrbuf, INET6_ADDRSTRLEN);
+	    fprintf(this->verbose_file, "Got neighbour solicitation from %s, looking for %s, has %u bytes of options\n",
+		    src_addrbuf,
+		    target_addrbuf,
+		    bytes_end - (u_char *)nd_options);
+	}
+    }
+    break;
+	
+    case ND_NEIGHBOR_ADVERT:
+    {
+	struct nd_neighbor_advert *nna = (struct nd_neighbor_advert *)bytes;
+	nd_options = (u_char *)&nna[1];
+	if(VERBOSE(this)) {
+	    char target_addrbuf[INET6_ADDRSTRLEN];
+	    inet_ntop(AF_INET6, &nna->nd_na_target, target_addrbuf, INET6_ADDRSTRLEN);
+	    fprintf(this->verbose_file, "Got neighbor advertisement from %s, advertising: %s, has %u bytes of options\n",
+		    src_addrbuf,
+		    target_addrbuf,
+		    bytes_end - (u_char *)nd_options);
+	}
+    }
+    break;
+    }
+
+    /* now decode neighbour discovery packets */
 }
 
 int network_interface::send_packet(const u_char *bytes, const int len)
