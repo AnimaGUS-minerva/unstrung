057b9f2bdc95ba59323e5ec04d109eb46b3a859e
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-09 23:16:01 -0500

    updated hexdump.c to elide properly

--- include/hexdump.c
+++ include/hexdump.c
@@ -20,11 +20,15 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 	unsigned char bb[4];             /* avoid doing byte accesses */
 	int i;
 	int first,last;     /* flags */
+	int elided=0;
 
 	last=0;
 	first=1;
   
 	for(i = 0; i < len; i++) {
+		/* see if we are at the last line */
+		if((len-i) <= 16) last=1;
+
 		/* if it's the first item on the line */
 		if((i % 16) == 0) {
 			/* and it's not the first or last line */
@@ -42,18 +46,19 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 					/* so just advance to next chunk,
 					 * noting the i++ above. */
 					i = i+15;
+					if(!elided) {
+						printf("...\n");
+						elided=1;
+					}
 					continue;
 				}
 			}
 			
-			/* see if we are at the last line */
-			if((len-i) < 16) last=1;
-			first=0;
-
 			/* print the offset */
 			hexdump_printf("%04x:", offset+i);
 		}
 
+		elided=0;
 		memcpy(bb, b+i, 4);
 		hexdump_printf(" %02x %02x %02x %02x ",
 			       bb[0], bb[1], bb[2], bb[3]);
@@ -61,8 +66,10 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 
 		/* see it's the last item on line */
 		if(!((i + 1) % 16)) {
+			first=0;
 			hexdump_printf("\n");
 		}
+
 	}
 	/* if it wasn't the last item on line */
 	if(i % 16) {
