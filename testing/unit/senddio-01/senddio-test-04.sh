b44e1a8a4e76fc6886831be04610d317bd3a340a
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-14 13:00:48 -0500

    #214 . also test long form of options

--- testing/unit/senddio-01/senddio-test-04.sh
+++ testing/unit/senddio-01/senddio-test-04.sh
@@ -2,4 +2,10 @@
 
 SENDDIO=${SENDDIO-./senddio}
 
-${SENDDIO} --fake -v -S 10 -p : | tee OUTPUT/senddio-test-04.raw | diff - senddio-test-04.out
+(
+${SENDDIO} --fake -v -S 10 -p :
+) | tee OUTPUT/senddio-test-04.raw | diff - senddio-test-04.out
+
+(
+${SENDDIO} --fake -v --sequence 10 --prefix :
+) | tee OUTPUT/senddio-test-04.raw | diff - senddio-test-04.out
