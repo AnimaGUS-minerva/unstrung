3021857c5e8367cc7e8ed72d6962d0e5a9aebb2a
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-14 18:08:16 -0500

    #214 . have hexdump program print the ascii version too

--- include/hexdump.c
+++ include/hexdump.c
@@ -11,12 +11,16 @@
  *
  */
 #include <string.h>
+#include <ctype.h>
 #ifndef hexdump_printf
 #define hexdump_printf printf
 #endif
 void hexdump(const unsigned char *base, unsigned int offset, int len)
 {
 	const unsigned char *b = base+offset;
+        char line[81];
+        char *out; int left=sizeof(line);
+        int outcount, posonthisline;
 	unsigned char bb[4];             /* avoid doing byte accesses */
 	int i;
 	int first,last;     /* flags */
@@ -24,13 +28,14 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 
 	last=0;
 	first=1;
+        posonthisline=0;
   
 	for(i = 0; i < len; i++) {
 		/* see if we are at the last line */
 		if((len-i) <= 16) last=1;
 
 		/* if it's the first item on the line */
-		if((i % 16) == 0) {
+		if(posonthisline == 0) {
 			/* and it's not the first or last line */
 			if(!first && !last) {
 				int j;
@@ -47,7 +52,7 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 					 * noting the i++ above. */
 					i = i+15;
 					if(!elided) {
-						printf("...\n");
+						hexdump_printf("...\n");
 						elided=1;
 					}
 					continue;
@@ -55,25 +60,45 @@ void hexdump(const unsigned char *base, unsigned int offset, int len)
 			}
 			
 			/* print the offset */
-			hexdump_printf("%04x:", offset+i);
+                        left=sizeof(line);
+                        out =line;
+                        memset(line, ' ', 79);
+                        line[80]='\0';
+                        outcount = snprintf(line, left, "%04x:", offset+i);
+                        left -= outcount;
+                        out  += outcount;
 		}
 
 		elided=0;
 		memcpy(bb, b+i, 4);
-		hexdump_printf(" %02x %02x %02x %02x ",
-			       bb[0], bb[1], bb[2], bb[3]);
-		i+=3;
+                outcount = snprintf(out, left, " %02x %02x %02x %02x ",
+                                    bb[0], bb[1], bb[2], bb[3]);
+                line[posonthisline+60]=(isprint(bb[0]) ? bb[0] : '.');
+                line[posonthisline+61]=(isprint(bb[1]) ? bb[1] : '.');
+                line[posonthisline+62]=(isprint(bb[2]) ? bb[2] : '.');
+                line[posonthisline+63]=(isprint(bb[3]) ? bb[3] : '.');
+                left -= outcount;
+                out  += outcount;
+		i    += 3;
+                posonthisline += 4;
 
 		/* see it's the last item on line */
 		if(!((i + 1) % 16)) {
 			first=0;
-			hexdump_printf("\n");
+                        *out=' ';
+                        hexdump_printf("%s\n",line);
+
+                        /* reset things */
+                        posonthisline=0;
+                        memset(line, ' ', 79);
+                        line[80]='\0';
 		}
 
 	}
 	/* if it wasn't the last item on line */
 	if(i % 16) {
-		hexdump_printf("\n");
+                *out=' ';
+                hexdump_printf("%s\n",line);
 	}
 }
 
