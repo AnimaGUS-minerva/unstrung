880f034dbe81d3e52496d08a60b32e32b842f85b
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-14 17:35:45 -0500

    #214 . include dagid, rank, sequence and instanceid in test case

--- testing/unit/senddio-01/senddio-test-04.sh
+++ testing/unit/senddio-01/senddio-test-04.sh
@@ -3,9 +3,10 @@
 SENDDIO=${SENDDIO-./senddio}
 
 (
-${SENDDIO} --fake -v -S 10 -p :
+${SENDDIO} --fake -v -p 2001:db8:0001::/48 -S 10 -I 42 -R 1 -D thisismynicedag1
 ) | tee OUTPUT/senddio-test-04.raw | diff - senddio-test-04.out
 
 (
-${SENDDIO} --fake -v --sequence 10 --prefix :
+${SENDDIO} --fake -v --prefix 2001:db8:0001::/48 --instance 42 \
+    --sequence 10 --rank 1 --dagid thisismynicedag1
 ) | tee OUTPUT/senddio-test-04.raw | diff - senddio-test-04.out
