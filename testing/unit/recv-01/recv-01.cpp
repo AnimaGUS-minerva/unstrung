e63e16d725d3285f492e050657893e4fb3f8656a
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-15 15:40:26 -0500

    #214 . recv-01 driver can now deal with DLT_LINUX_SLL encapsulated save files

--- testing/unit/recv-01/recv-01.cpp
+++ testing/unit/recv-01/recv-01.cpp
@@ -4,6 +4,8 @@
 
 extern "C" {
 #include "pcap.h"
+#include "sll.h"
+#include "ether.h"
 #include <libgen.h>
 #include <string.h>
 #include <netinet/ip6.h>
@@ -14,6 +16,8 @@ extern "C" {
 class pcap_network_interface : public network_interface {
 public:
 	pcap_network_interface(pcap_dumper_t *pd);
+        virtual void skip_pcap_headers(const struct pcap_pkthdr *h,
+                                       const u_char *bytes);
 	int send_packet(const u_char *bytes, const int len);
 	void increment_packet(void)   { packet_count++; };
 	unsigned int packet_num(void) { return packet_count; };
@@ -25,6 +29,8 @@ private:
 
 class pcap_linux_network_interface : public pcap_network_interface {
 public:
+        virtual void skip_pcap_headers(const struct pcap_pkthdr *h,
+                                       const u_char *bytes);
 	pcap_linux_network_interface(pcap_dumper_t *pd);
 };
 
@@ -51,23 +57,29 @@ int pcap_network_interface::send_packet(const u_char *bytes, const int len)
 	pcap_dump((u_char *)this->pcap_out, &h, bytes);
 }
 
-
 void sunshine_pcap_input(u_char *u,
 			 const struct pcap_pkthdr *h,
 			 const u_char *bytes)
 {
 	pcap_network_interface *pnd = (pcap_network_interface *)u;
+
+        pnd->skip_pcap_headers(h, bytes);
+}
+
+void pcap_network_interface::skip_pcap_headers(const struct pcap_pkthdr *h,
+                                               const u_char *bytes)
+{
 	int len = h->len;
 
 	/* validate input packet a bit, before passing to receive layer */
 	/* first, check ethernet, and reject non EthernetII frames 
 	 * from Unit testing.
 	 */
-	pnd->increment_packet();
+	this->increment_packet();
 
 	if(bytes[12]!=0x86 || bytes[13]!=0xdd) {
 		printf("packet %u not ethernet II (is: %02x%02x), dropped\n",
-		       pnd->packet_num(), bytes[12], bytes[13]);
+		       this->packet_num(), bytes[12], bytes[13]);
 		return;
 	}
 	
@@ -84,7 +96,7 @@ void sunshine_pcap_input(u_char *u,
 	unsigned int nh = ip6->ip6_nxt;       /* type of next PDU */
 
 	if(nh != IPPROTO_ICMPV6) {
-		printf("packet %u is not ICMPv6, but=proto:%u\n", pnd->packet_num(), nh);
+		printf("packet %u is not ICMPv6, but=proto:%u\n", this->packet_num(), nh);
 		return;
 	}
 
@@ -97,12 +109,111 @@ void sunshine_pcap_input(u_char *u,
 	   icmp6->icmp6_type != ND_NEIGHBOR_SOLICIT &&
 	   icmp6->icmp6_type != ND_NEIGHBOR_ADVERT) {
 		printf("packet %u is not ICMPv6, but=proto:%u\n",
-		       pnd->packet_num(), icmp6->icmp6_type);
+		       this->packet_num(), icmp6->icmp6_type);
 		return;
 	}
 
-	pnd->receive_packet(ip6->ip6_src, ip6->ip6_dst, bytes, len);
+	this->receive_packet(ip6->ip6_src, ip6->ip6_dst, bytes, len);
 }
+
+
+void pcap_linux_network_interface::skip_pcap_headers(const struct pcap_pkthdr *h,
+                                                     const u_char *p)
+{
+	u_int caplen = h->caplen;
+	u_int len = h->len;
+	const struct sll_header *sllp;
+	u_short ether_type;
+	u_short extracted_ethertype;
+        const unsigned char *bytes;
+
+	this->increment_packet();
+
+	if (caplen < SLL_HDR_LEN) {
+		/*
+		 * XXX - this "can't happen" because "pcap-linux.c" always
+		 * adds this many bytes of header to every packet in a
+		 * cooked socket capture.
+		 */
+		return;
+	}
+
+	sllp = (const struct sll_header *)p;
+
+	/*
+	 * Go past the cooked-mode header.
+	 */
+	len    -= SLL_HDR_LEN;
+	caplen -= SLL_HDR_LEN;
+	p      += SLL_HDR_LEN;
+
+	ether_type = ntohs(sllp->sll_protocol);
+
+	/*
+	 * Is it (gag) an 802.3 encapsulation, or some non-Ethernet
+	 * packet type?
+	 */
+	if (ether_type <= ETHERMTU) {
+		/*
+		 * Yes - what type is it?
+		 */
+		switch (ether_type) {
+		case LINUX_SLL_P_802_2:
+                        bytes = p;
+                        /* not correct */
+                        
+			break;
+
+		default:
+                        printf("packet ether_type: %024 rejected\n",
+                               ether_type);
+                        return;
+		}
+	} else {
+                /* validate input packet a bit, before passing to receive
+                 * layer, check ethernet, and reject non EthernetII frames 
+                 * from Unit testing.
+                 */
+
+                if(ether_type != 0x86dd) {
+                        printf("packet %u not ethernet II (is: %04x), dropped\n",
+                               this->packet_num(), ether_type);
+                        return;
+                }
+                bytes = p;
+        }
+	
+	/* make sure it's IPv6! */
+	if((bytes[0] & 0xf0) != 0x60) {
+		printf("packet %u is not IPv6: %u\n", bytes[14] >> 4);
+		return;
+	}
+
+	struct ip6_hdr *ip6 = (struct ip6_hdr *)bytes;
+	unsigned int nh = ip6->ip6_nxt;       /* type of next PDU */
+
+	if(nh != IPPROTO_ICMPV6) {
+		printf("packet %u is not ICMPv6, but=proto:%u\n", this->packet_num(), nh);
+		return;
+	}
+
+	bytes += sizeof(struct ip6_hdr);  /* 40 bytes */
+	len   -= sizeof(struct ip6_hdr);
+
+	struct icmp6_hdr *icmp6 = (struct icmp6_hdr *)bytes;
+	if(icmp6->icmp6_type != ND_ROUTER_SOLICIT &&
+	   icmp6->icmp6_type != ND_ROUTER_ADVERT  &&
+	   icmp6->icmp6_type != ND_NEIGHBOR_SOLICIT &&
+	   icmp6->icmp6_type != ND_NEIGHBOR_ADVERT  &&
+           icmp6->icmp6_type != ND_RPL_MESSAGE) {
+		printf("packet %u is not ND, but=proto:%u\n",
+		       this->packet_num(), icmp6->icmp6_type);
+		return;
+	}
+
+	this->receive_packet(ip6->ip6_src, ip6->ip6_dst, bytes, len);
+}
+
 
 int process_infile(char *infile, char *outfile)
 {
@@ -115,7 +226,8 @@ int process_infile(char *infile, char *outfile)
 		exit(1);
 	}
 
-	pcap_t *pout = pcap_open_dead(pcap_datalink(pol), 65535);
+        int pcap_link = pcap_datalink(pol);
+	pcap_t *pout = pcap_open_dead(pcap_link, 65535);
 	if(!pout) {
 		fprintf(stderr, "can not create pcap_open_deads\n");
 		exit(1);
@@ -128,7 +240,7 @@ int process_infile(char *infile, char *outfile)
 		exit(1);
 	}
 
-        switch(pcap_datalink(pol)) {
+        switch(pcap_link) {
         case DLT_LINUX_SLL:
                 ndproc = new pcap_linux_network_interface(out);
                 break;
@@ -139,8 +251,7 @@ int process_infile(char *infile, char *outfile)
                 
         default:
                 fprintf(stderr, "unimplemented dlt type: %s (%u)",
-                        pcap_datalink_val_to_name(pcap_datalink(pol)),
-                        pcap_datalink(pol));
+                        pcap_datalink_val_to_name(pcap_link),pcap_link);
                 exit(1);
         }
 
