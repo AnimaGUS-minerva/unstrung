59cf58313359379fbcca70e0b9d415381a17e5d7
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-15 21:56:38 -0500

    #214 . added library routines for parsing addresses, etc. from openswan

---
+++ include/oswlibs.h
@@ -0,0 +1,102 @@
+/*
+ * internal definitions for use within the library; do not export!
+ * Copyright (C) 1998, 1999  Henry Spencer.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/lgpl.txt>.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ *
+ * RCSID $Id: internal.h,v 1.12 2004/04/11 17:08:24 mcr Exp $
+ */
+
+#ifndef _OSWLIBS_H_
+
+#ifndef ABITS
+#define	ABITS	32	/* bits in an IPv4 address */
+#endif
+
+/* case-independent ASCII character equality comparison */
+#define	CIEQ(c1, c2)	( ((c1)&~040) == ((c2)&~040) )
+
+/*
+ * Headers, greatly complicated by stupid and unnecessary inconsistencies
+ * between the user environment and the kernel environment.  These are done
+ * here so that this mess need exist in only one place.
+ *
+ * It may seem like a -I or two could avoid most of this, but on closer
+ * inspection it is not quite that easy.
+ */
+
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+
+/* You'd think this would be okay in the kernel too -- it's just a */
+/* bunch of constants -- but no, in RH5.1 it screws up other things. */
+/* (Credit:  Mike Warfield tracked this problem down.  Thanks Mike!) */
+/* Fortunately, we don't need it in the kernel subset of the library. */
+#include <limits.h>
+
+/* header files for things that should never be called in kernel */
+#include <netdb.h>
+
+/* memory allocation, currently user-only, macro-ized just in case */
+#include <stdlib.h>
+#define	MALLOC(n)	malloc(n)
+#define	FREE(p)		free(p)
+
+
+typedef const char *err_t;	/* error message, or NULL for success */
+typedef struct {
+	union {
+		struct sockaddr_in v4;
+		struct sockaddr_in6 v6;
+	} u;
+} ip_address;
+
+typedef struct {
+	ip_address addr;
+	int maskbits;
+} ip_subnet;
+
+extern err_t ttosubnet(const char *src, size_t srclen, int af, ip_subnet *dst);
+/* looks up names in DNS */
+extern err_t ttoaddr(const char *src, size_t srclen, int af, ip_address *dst);
+extern err_t tnatoaddr(const char *src, size_t srclen, int af, ip_address *dst);
+
+extern void setportof(int port, ip_address *dst);
+extern err_t ttoul(const char *src, size_t srclen, int format, unsigned long *dst);
+extern int masktocount(const ip_address *src);
+extern err_t initsubnet(const ip_address *addr, int maskbits, int clash, ip_subnet *dst);
+extern size_t addrbytesptr(const ip_address *src, const unsigned char **dst);
+extern size_t addrbytesptr_write(ip_address *src, unsigned char **dst);
+extern size_t addrbytesof(const ip_address *src, unsigned char *dst, size_t dstlen);
+extern int portof(const ip_address *src);
+extern void setportof(int port, ip_address *dst);
+extern err_t initaddr(const unsigned char *src, size_t srclen, int af, ip_address *dst);
+extern err_t unspecaddr(int af, ip_address *dst);
+extern int addrtypeof(const ip_address *src);
+extern size_t addrlenof(const ip_address *src);
+extern err_t addrtosubnet(const ip_address *addr, ip_subnet *dst);
+extern struct sockaddr *sockaddrof(ip_address *src);
+extern size_t sockaddrlenof(const ip_address *src);
+extern int subnettypeof(const ip_subnet *src);
+extern void networkof(const ip_subnet *src, ip_address *dst);
+extern void maskof(const ip_subnet *src, ip_address *dst);
+extern err_t add_port(int af, ip_address *addr, unsigned short port);
+extern err_t anyaddr(int af, ip_address *dst);
+extern err_t loopbackaddr(int af, ip_address *dst);
+extern int isanyaddr(const ip_address *src);
+extern int isunspecaddr(const ip_address *src);
+extern int isloopbackaddr(const ip_address *src);
+
+#define _OSWLIBS_H_ 
+#endif /* _OSWLIBS_H_ */
---
+++ lib/libosw/initaddr.c
@@ -0,0 +1,70 @@
+/*
+ * initialize address structure
+ * Copyright (C) 2000  Henry Spencer.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/lgpl.txt>.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ *
+ * RCSID $Id: initaddr.c,v 1.6 2004/07/10 07:43:47 mcr Exp $
+ */
+#include "oswlibs.h"
+
+err_t
+add_port(af, addr, port)
+int af;
+ip_address *addr;
+unsigned short port;
+{
+	switch (af) {
+	case AF_INET:
+		addr->u.v4.sin_port = port;
+		break;
+	case AF_INET6:
+		addr->u.v6.sin6_port = port;
+		break;
+	default:
+		return "unknown address family in add_port";
+		break;
+	}
+	return NULL;
+}
+
+/*
+ - initaddr - initialize ip_address from bytes
+ */
+err_t				/* NULL for success, else string literal */
+initaddr(src, srclen, af, dst)
+const unsigned char *src;
+size_t srclen;
+int af;				/* address family */
+ip_address *dst;
+{
+	switch (af) {
+	case AF_INET:
+		if (srclen != 4)
+			return "IPv4 address must be exactly 4 bytes";
+		dst->u.v4.sin_family = af;
+		dst->u.v4.sin_port = 0;
+		memcpy((char *)&dst->u.v4.sin_addr.s_addr, src, srclen);
+		break;
+	case AF_INET6:
+		if (srclen != 16)
+			return "IPv6 address must be exactly 16 bytes";
+		dst->u.v6.sin6_family = af;
+		dst->u.v6.sin6_flowinfo = 0;		/* unused */
+		dst->u.v6.sin6_port = 0;
+		memcpy((char *)&dst->u.v6.sin6_addr, src, srclen);
+		break;
+	default:
+		return "unknown address family in initaddr";
+		break;
+	}
+	return NULL;
+}
