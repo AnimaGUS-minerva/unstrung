5083400cb3699f988eac94f6438d75a832df036c
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2010-03-22 19:04:00 -0400

    #270 . have localswitches setup pcal files

--- testing/utils/localswitches.tcl
+++ testing/utils/localswitches.tcl
@@ -43,17 +43,24 @@ close $switchvars
 #    system "$host single &"
 #}
 
-#foreach net $managednets {
-#    if {[info exists umlid(net$net,play)] } {
-#	puts "Will play pcap file $umlid(net$net,play) to network '$net'\r\n"
-#	setupplay $netjig1 $net $umlid(net$net,play) "--rate=ontick"
-#    }
-#}
+foreach net $managednets {
+    if {[info exists umlid(net$net,play)] } {
+	puts "Will play pcap file $umlid(net$net,play) to network '$net'\r\n"
+	setupplay $netjig1 $net $umlid(net$net,play) "--rate=ontick"
+    }
+}
 
 puts "\r\nExit the netjig when you are done\r\n"
 
 set timeout -1
-interact -reset -echo -i $netjig1 
+interact {
+    -reset
+    -echo
+    -i $netjig1
+    "\r" { send_user "\r\n"
+           exp_send "\n"
+    }
+}
 
 foreach host $argv {
     system "uml_mconsole $host halt"
