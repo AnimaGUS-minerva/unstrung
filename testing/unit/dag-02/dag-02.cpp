b6df689c90a2c1d9ffb37d409f91c4800bb80834
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2010-03-23 00:35:08 -0400

    #270 . updated test cases for receive_dio() signature change

--- testing/unit/dag-02/dag-02.cpp
+++ testing/unit/dag-02/dag-02.cpp
@@ -19,10 +19,14 @@ extern "C" {
 #include <netinet/ip6.h>
 #include <netinet/icmp6.h>
 #include <assert.h>
+#include <time.h>
 }
 
 #include "iface.h"
 #include "dag.h"
+
+struct in6_addr dummy_src1;
+time_t now;
 
 class dag_network *dn = NULL;
 
@@ -44,43 +48,43 @@ static void t2(void)
         d1.rpl_dagid[0]='1';
 
         d1.rpl_seq = 1;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 1);
         assert(dn->mStats[PS_SEQ_OLD] == 0);
 
         d1.rpl_seq = 4;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 4);
         assert(dn->mStats[PS_SEQ_OLD] == 0);
 
         d1.rpl_seq = 3;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 4);
         assert(dn->mStats[PS_SEQ_OLD] == 1);
 
         d1.rpl_seq = 240;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 4);
         assert(dn->mStats[PS_SEQ_OLD] == 2);
 
         d1.rpl_seq = 130;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 130);
         assert(dn->mStats[PS_SEQ_OLD] == 2);
 
         d1.rpl_seq = 243;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 243);
         assert(dn->mStats[PS_SEQ_OLD] == 2);
 
         d1.rpl_seq = 1;
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 1);
         assert(dn->mStats[PS_SEQ_OLD] == 2);
 
         d1.rpl_seq = 1;
         unsigned int processed_count = dn->mStats[PS_PACKET_PROCESSED];
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->last_seq() == 1);
         assert(dn->mStats[PS_PACKET_PROCESSED] == processed_count+1);
 }
@@ -107,7 +111,7 @@ static void t3(void)
 
         unsigned int lower_rank_count = dn->mStats[PS_LOWER_RANK_CONSIDERED];
 
-        dn->receive_dio(&d1, sizeof(d1));
+        dn->receive_dio(dummy_src1, now, &d1, sizeof(d1));
         assert(dn->mStats[PS_LOWER_RANK_CONSIDERED] == lower_rank_count+1);
 }
         
@@ -115,6 +119,11 @@ static void t3(void)
 int main(int argc, char *argv[])
 {
 	int i;
+
+        struct in6_addr dummy_src1;
+        inet_pton(AF_INET6, "2001:db8::abcd:00a1", &dummy_src1);
+        time_t now;
+        time(&now);
 
         dagid_t d;
         memset(d, 0, DAGID_LEN);
