8ace64e16067512ae005f95d30ee39bb1835e571
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2010-04-12 22:58:58 -0400

    #270 . break open the DIO options

---
+++ include/dio.h
@@ -0,0 +1,44 @@
+#ifndef _UNSTRUNG_DIO_H_
+#define _UNSTRUNG_DIO_H_
+
+extern "C" {
+#include <errno.h>
+#include <pathnames.h>		/* for PATH_PROC_NET_IF_INET6 */
+#include <arpa/inet.h>
+#include <netinet/ip6.h>
+#include <string.h>
+#include "rpl.h"
+}
+
+#include "node.h"
+
+class rpl_dio {
+public:
+    rpl_dio(rpl_node &peer,
+            const struct nd_rpl_dio *dio, int dio_len);
+    struct nd_rpl_genoption *search_subopt(enum RPL_DIO_SUBOPT optnum);
+    struct rpl_dio_destprefix *destprefix(void);
+    void   reset_options(void);
+    
+private:
+    rpl_node                &mPeer;
+    u_int8_t                mBytes[2048];
+    int                     mLen;
+
+    /*
+     * this structure is a set of virtual pointers,
+     * where we last found an an option of that particular type.
+     */
+    int16_t                  mOptions[256]; 
+};
+
+
+#endif /* NODE_H */
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: whitesmith
+ * End:
+ */
+
