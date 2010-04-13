76ac3810b16f7b957204375b168241b810e272c6
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2010-04-13 00:07:40 -0400

    #270 . include addrtot codes

--- include/oswlibs.h
+++ include/oswlibs.h
@@ -98,6 +98,16 @@ extern int isanyaddr(const ip_address *src);
 extern int isunspecaddr(const ip_address *src);
 extern int isloopbackaddr(const ip_address *src);
 
+size_t addrtot(const ip_address *src, int format, char *buf, size_t buflen);
+#define	ADDRTOT_BUF	(32*2 + 3 + 1 + 3 + 1 + 1)
+
+size_t subnettot(const ip_subnet *src, int format, char *buf, size_t buflen);
+#define	SUBNETTOT_BUF	(ADDRTOT_BUF + 1 + 3)
+
+size_t ultot(unsigned long src, int format, char *buf, size_t buflen);
+#define	ULTOT_BUF	(22+1)	/* holds 64 bits in octal */
+
+
 #define _OSWLIBS_H_ 
 #endif /* _OSWLIBS_H_ */
 
