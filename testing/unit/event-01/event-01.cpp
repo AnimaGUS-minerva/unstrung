c440fc8d35a2c00fe974b9125e755111b19fd9a5
Michael Richardson <mcr@sandelman.ca>
2010-07-19 00:01:47 -0400

    added test case for tv_sec being non-zero, should be last

--- testing/unit/event-01/event-01.cpp
+++ testing/unit/event-01/event-01.cpp
@@ -31,12 +31,14 @@ static void t1(void)
     rpl_event e1(0, 300, "e1");
     rpl_event e2(0, 500, "e2");
     rpl_event e3(0, 150, "e3");
+    rpl_event e4(1, 150, "e4");
 
     event_map n;
 
     n[e1.interval] = e1;
     n[e2.interval] = e2;
     n[e3.interval] = e3;
+    n[e4.interval] = e4;
 
     event_map_iterator first = n.begin();
     assert(first != n.end());
@@ -45,6 +47,22 @@ static void t1(void)
     printf("first interval: %u %s\n",
            n1.interval.tv_usec, n1.mReason);
     assert(n1.interval.tv_usec == 150000);
+
+#if 0
+    /* we want the last item, but n.end() is the one after that one */
+    /* need to read the STL manual */
+    event_map_iterator last = n.end();
+    assert(last != n.begin());
+    
+    rpl_event &n2 = last->second;
+    printf("last interval: %u %u %s\n",
+           n2.interval.tv_sec,
+           n2.interval.tv_usec,
+           n2.mReason);
+    assert(n2.interval.tv_sec  == 1);
+    assert(n2.interval.tv_usec == 150000);
+#endif
+    
 }
 
 
