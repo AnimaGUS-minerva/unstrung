4d56bfc155a3abb3e3c9ddbb498e3b2713a84ed5
Michael Richardson <mcr@sandelman.ca>
2010-07-18 22:01:14 -0400

    test case that includes adding a new prefix to the interface when DIO received

--- testing/functional/recv-03/testparams.sh
+++ testing/functional/recv-03/testparams.sh
@@ -14,7 +14,8 @@ N1_PLAY=../../unit/INPUTS/dio-02.pcap
 REF_N1_OUTPUT=n1-output.txt
 REF_N1_FILTER="no-icmp-v6-mcast.sed"
 REF_E_CONSOLE_OUTPUT=econsole.txt
-REF_CONSOLE_FIXUPS="script-only.sed"
+REF_CONSOLE_FIXUPS="script-only.sed "
+REF_CONSOLE_FIXUPS="${REF_CONSOLE_FIXUPS} remove_dummy.sed"
 E_RUN_SCRIPT=sunshine.sh
 E_FINAL_SCRIPT=sunfinal.sh
 
