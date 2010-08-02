39e6d215e2627c0f6efa71e035ae7cbd73e568b4
Michael Richardson <mcr@sandelman.ca>
2010-08-02 17:49:02 -0400

    fixed instability in tcpdump filter output

--- testing/functional/recv-02/testparams.sh
+++ testing/functional/recv-02/testparams.sh
@@ -15,6 +15,7 @@ REF_N1_OUTPUT=n1-output.txt
 REF_N1_FILTER="no-icmp-v6-mcast.sed"
 REF_E_CONSOLE_OUTPUT=econsole.txt
 REF_CONSOLE_FIXUPS="script-only.sed"
+REF_CONSOLE_FIXUPS="${REF_CONSOLE_FIXUPS} tcpdump-packet-count.sed"
 REF_CONSOLE_FIXUPS="${REF_CONSOLE_FIXUPS} no-icmp-v6-mcast.sed"
 E_RUN_SCRIPT=sunshine.sh
 E_FINAL_SCRIPT=sunfinal.sh
---
+++ testing/utils/fixups/tcpdump-packet-count.sed
@@ -0,0 +1,4 @@
+#!/bin/sed -f
+
+/[0-9]* packets captured/d
+/[0-9]* packets received by filter/d
