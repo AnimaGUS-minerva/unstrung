c81288ff96a04a790954f8ad190df6c5e053b9b4
Michael Richardson <mcr@sandelman.ca>
2010-08-02 14:44:35 -0400

    instead of creating an iface directly, scan available devices and then look for the device of the right name also add options to set rpl_interval, instanceid and dagrank

--- programs/sunshine/main.cpp
+++ programs/sunshine/main.cpp
@@ -29,6 +29,12 @@ static struct option const longopts[] =
     { "help",      0, 0, '?'}, 
     { "interface", 0, 0, 'i'}, 
     { "daemon",    0, 0, 'D'}, 
+    { "prefix",    1, NULL, 'p'},
+    { "instance",  1, NULL, 'I'},
+    { "interval",  1, NULL, 'W'},
+    { "dagid",     1, NULL, 'G'},
+    { "dagid",     1, NULL, 'G'},
+    { "rank",      1, NULL, 'R'},
     { "kill",      0, 0, 'K'}, 
     { "verbose",   0, 0, 'v'}, 
     { name: 0 }, 
@@ -37,6 +43,13 @@ static struct option const longopts[] =
 void usage()
 {
     fprintf(stderr, "Usage: %s [-?] [-i ifname]\n", progname);
+    fprintf(stderr,
+            "\t [-p prefix] [--prefix prefix]   announce this IPv6 prefix in the destination option\n"
+            "\t [-G dagid]  [--dagid dagid]     DAGid to use to announce, string or hexn\n"
+            "\t [-R rank]   [--rank rank]       Initial rank to announce with\n"
+            "\t [-I num]    [--instanceid num]  Instance ID (number)\n"
+            "\t [-W msec]   [--interval msec]   Number of miliseconds between DIO\n"
+        );
     exit(EX_USAGE);
 }
 
@@ -77,7 +90,12 @@ int main(int argc, char *argv[])
     bool bedaemon = false;
 
     progname = argv[0];
-    while((c = getopt_long(argc, argv, "KDi:h?v", longopts, 0)) != EOF) {
+    network_interface::scan_devices();
+
+    network_interface *iface = NULL;
+
+
+    while((c = getopt_long(argc, argv, "KDG:I:R:W:i:hp:?v", longopts, 0)) != EOF) {
 	switch(c) {
 	default:
 	    fprintf(stderr, "Unknown option: %s\n", argv[optind-1]);
@@ -95,22 +113,46 @@ int main(int argc, char *argv[])
             killanydaemon();
             exit(0);
 
+        case 'p':
+        {
+            ip_subnet prefix;
+            
+            err_t e = ttosubnet(optarg, strlen(optarg),
+                                AF_INET6, &prefix);
+
+            iface->set_rpl_prefix(prefix);
+        }
+        break;
+
+        case 'I':
+            iface->set_rpl_instanceid(atoi(optarg));
+            break; 
+
+        case 'W':
+            iface->set_rpl_interval(atoi(optarg));
+            break; 
+
+        case 'R':
+            iface->set_rpl_dagrank(atoi(optarg));
+            break; 
+
+        case 'G':
+            iface->set_rpl_dagid(optarg);
+            break;
+
         case 'v':
             verbose++;
             break;
+
         case 'i':
-            ifname = optarg;
+            iface = network_interface::find_by_name(optarg);
             break;
 	}
     }
 
-    if(!ifname) usage();
+    if(!iface) usage();
 
-    network_interface::scan_devices();
-
-    network_interface *sc = network_interface::find_by_name(ifname);
-    sc->setup();
-    sc->set_verbose(verbose, stderr);
+    if(iface) iface->set_verbose(verbose, stderr);
 
     /* should check for already running instance before stomping PID file */
 
