688f8d2525958d37bbae5c887a998d382d19e358
Michael Richardson <mcr@sandelman.ca>
2010-12-29 23:19:45 -0500

    created dao routines

---
+++ include/dao.h
@@ -0,0 +1,44 @@
+#ifndef _UNSTRUNG_DIO_H_
+#define _UNSTRUNG_DIO_H_
+
+extern "C" {
+#include <errno.h>
+#include <pathnames.h>		/* for PATH_PROC_NET_IF_INET6 */
+#include <arpa/inet.h>
+#include <netinet/ip6.h>
+#include <string.h>
+#include "rpl.h"
+}
+
+#include "node.h"
+
+class rpl_dao {
+public:
+    rpl_dao(rpl_node &peer,
+            const struct nd_rpl_dao *dao, int dao_len);
+    struct nd_rpl_genoption *search_subopt(enum RPL_DIO_SUBOPT optnum,
+                                           int *p_opt_len = NULL);
+    void   reset_options(void);
+    
+private:
+    rpl_node                &mPeer;
+    u_int8_t                mBytes[2048];
+    int                     mLen;
+
+    /*
+     * this structure is a set of virtual pointers,
+     * where we last found an an option of that particular type.
+     */
+    int16_t                  mOptions[256]; 
+};
+
+
+#endif /* NODE_H */
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: whitesmith
+ * End:
+ */
+
---
+++ lib/libndmgmt/dao.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2009 Michael Richardson <mcr@sandelman.ca>
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C" {
+#include <errno.h>
+#include <pathnames.h>		/* for PATH_PROC_NET_IF_INET6 */
+#include <time.h>
+#include <arpa/inet.h>
+#include <netinet/ip6.h>
+#include <netinet/icmp6.h>
+#include <linux/if.h>           /* for IFNAMSIZ */
+#include "oswlibs.h"
+#include "rpl.h"
+
+}
+
+#include "iface.h"
+#include "dag.h"
+#include "dao.h"
+
+void network_interface::receive_dao(const u_char *dao, const int dao_len)
+{
+        if(VERBOSE(this))
+                fprintf(this->verbose_file, " processing dao(%u)\n",dao_len);
+        
+}
+
+
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: whitesmith
+ * End:
+ */
