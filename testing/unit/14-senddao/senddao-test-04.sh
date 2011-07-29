0cd493a8eed7b026f247ed2731879416350ea069
Michael Richardson <mcr@sandelman.ca>
2011-07-29 12:55:00 -0400

    #2909 . turn off some disabled tests

--- testing/unit/14-senddao/senddao-test-04.sh
+++ testing/unit/14-senddao/senddao-test-04.sh
@@ -6,4 +6,5 @@ PCAP04=../OUTPUTS/senddao-test-04out.pcap
 ${SENDDAO} --fake --outpcap ${PCAP04} -i wlan0 -v --dagid thisismydicedag2 --sequence 11 --instance 43 --prefix fdfd:abcd:ef01::/48 2>&1 | 
   tee ../OUTPUTS/senddao-test-04.raw | diff - senddao-test-04.out
 
-tcpdump -t -n -r ${PCAP04} -v -v | tee ../OUTPUTS/senddao-test-04-pcap.txt | diff - senddao-test-04-pcap.txt
+# disabled #2901
+#tcpdump -t -n -r ${PCAP04} -v -v | tee ../OUTPUTS/senddao-test-04-pcap.txt | diff - senddao-test-04-pcap.txt
