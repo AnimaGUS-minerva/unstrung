55687a94b2f6fc139ce328d955d219124e1547d3
Michael Richardson <mcr@sandelman.ca>
2012-08-23 11:42:23 -0400

    whitespace changes only

--- include/hexdump.c
+++ include/hexdump.c
@@ -17,89 +17,89 @@
 #endif
 static void hexdump(const unsigned char *base, unsigned int offset, int len)
 {
-	const unsigned char *b = base+offset;
-        char line[81];
-        char *out; int left=sizeof(line);
-        int outcount, posonthisline;
-	unsigned char bb[4];             /* avoid doing byte accesses */
-	int i;
-	int first,last;     /* flags */
-	int elided=0;
+    const unsigned char *b = base+offset;
+    char line[81];
+    char *out; int left=sizeof(line);
+    int outcount, posonthisline;
+    unsigned char bb[4];             /* avoid doing byte accesses */
+    int i;
+    int first,last;     /* flags */
+    int elided=0;
 
-	last=0;
-	first=1;
-        posonthisline=0;
-  
-	for(i = 0; i < len; i++) {
-		/* see if we are at the last line */
-		if((len-i) <= 16) last=1;
+    last=0;
+    first=1;
+    posonthisline=0;
 
-		/* if it's the first item on the line */
-		if(posonthisline == 0) {
-			/* and it's not the first or last line */
-			if(!first && !last) {
-				int j;
+    for(i = 0; i < len; i++) {
+        /* see if we are at the last line */
+        if((len-i) <= 16) last=1;
 
-				/* see if all the entries are zero */
-				for(j=0; j < 4; j++) {
-					memcpy(bb, b+i+4*j, 4);
-					if(bb[0] || bb[1] || bb[2] || bb[3]) break;
-				}
+        /* if it's the first item on the line */
+        if(posonthisline == 0) {
+            /* and it's not the first or last line */
+            if(!first && !last) {
+                int j;
 
-				/* yes, they all are */
-				if(j==4) {
-					/* so just advance to next chunk,
-					 * noting the i++ above. */
-					i = i+15;
-					if(!elided) {
-						hexdump_printf("...\n");
-						elided=1;
-					}
-					continue;
-				}
-			}
-			
-			/* print the offset */
-                        left=sizeof(line);
-                        out =line;
-                        memset(line, ' ', 79);
-                        line[80]='\0';
-                        outcount = snprintf(line, left, "%04x:", offset+i);
-                        left -= outcount;
-                        out  += outcount;
-		}
+                /* see if all the entries are zero */
+                for(j=0; j < 4; j++) {
+                    memcpy(bb, b+i+4*j, 4);
+                    if(bb[0] || bb[1] || bb[2] || bb[3]) break;
+                }
 
-		elided=0;
-		memcpy(bb, b+i, 4);
-                outcount = snprintf(out, left, " %02x %02x %02x %02x ",
-                                    bb[0], bb[1], bb[2], bb[3]);
-                line[posonthisline+60]=(isprint(bb[0]) ? bb[0] : '.');
-                line[posonthisline+61]=(isprint(bb[1]) ? bb[1] : '.');
-                line[posonthisline+62]=(isprint(bb[2]) ? bb[2] : '.');
-                line[posonthisline+63]=(isprint(bb[3]) ? bb[3] : '.');
-                left -= outcount;
-                out  += outcount;
-		i    += 3;
-                posonthisline += 4;
+                /* yes, they all are */
+                if(j==4) {
+                    /* so just advance to next chunk,
+                     * noting the i++ above. */
+                    i = i+15;
+                    if(!elided) {
+                        hexdump_printf("...\n");
+                        elided=1;
+                    }
+                    continue;
+                }
+            }
 
-		/* see it's the last item on line */
-		if(!((i + 1) % 16)) {
-			first=0;
-                        *out=' ';
-                        hexdump_printf("%s\n",line);
+            /* print the offset */
+            left=sizeof(line);
+            out =line;
+            memset(line, ' ', 79);
+            line[80]='\0';
+            outcount = snprintf(line, left, "%04x:", offset+i);
+            left -= outcount;
+            out  += outcount;
+        }
 
-                        /* reset things */
-                        posonthisline=0;
-                        memset(line, ' ', 79);
-                        line[80]='\0';
-		}
+        elided=0;
+        memcpy(bb, b+i, 4);
+        outcount = snprintf(out, left, " %02x %02x %02x %02x ",
+                            bb[0], bb[1], bb[2], bb[3]);
+        line[posonthisline+60]=(isprint(bb[0]) ? bb[0] : '.');
+        line[posonthisline+61]=(isprint(bb[1]) ? bb[1] : '.');
+        line[posonthisline+62]=(isprint(bb[2]) ? bb[2] : '.');
+        line[posonthisline+63]=(isprint(bb[3]) ? bb[3] : '.');
+        left -= outcount;
+        out  += outcount;
+        i    += 3;
+        posonthisline += 4;
 
-	}
-	/* if it wasn't the last item on line */
-	if(i % 16) {
-                *out=' ';
-                hexdump_printf("%s\n",line);
-	}
+        /* see it's the last item on line */
+        if(!((i + 1) % 16)) {
+            first=0;
+            *out=' ';
+            hexdump_printf("%s\n",line);
+
+            /* reset things */
+            posonthisline=0;
+            memset(line, ' ', 79);
+            line[80]='\0';
+        }
+
+    }
+    /* if it wasn't the last item on line */
+    if(i % 16) {
+        *out=' ';
+        hexdump_printf("%s\n",line);
+    }
 }
 
 /*
