0dcd8a673abc58b96215498743eac439c79e9222
Michael Richardson <mcr@sandelman.ca>
2013-01-04 23:39:53 -0500

    fix 02-recvdio: set fake time on events on -03 and -04 test cases, dagid is different, so no new node is recognized

--- testing/unit/02-recvdio/recv-01.cpp
+++ testing/unit/02-recvdio/recv-01.cpp
@@ -11,9 +11,17 @@ int main(int argc, char *argv[])
         
         pcap_network_interface::scan_devices(deb);
 
+	struct timeval n;
+	n.tv_sec = 1024*1024*1024;
+	n.tv_usec = 1024;
+
+	class network_interface *iface = network_interface::find_by_ifindex(1);
+	iface->set_fake_time(n);
+
 	/* setup DAG as interesting */
 	dag_network *d1 = dag_network::find_or_make_by_string("thisismynicedag1", deb, false);
 	d1->set_active();
+	d1->set_interval(500);
 
 	for(i = 1; i < argc; i++) {
 		char b1[256];
