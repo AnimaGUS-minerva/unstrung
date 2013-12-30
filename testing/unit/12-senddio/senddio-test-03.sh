0c8f4232331f2bdc5cb6ce4d8cf2083627036ae9
Michael Richardson <mcr@sandelman.ca>
2013-12-29 20:42:31 -0500

    make test 12, part-03 ignore whitespace

--- testing/unit/12-senddio/senddio-test-03.sh
+++ testing/unit/12-senddio/senddio-test-03.sh
@@ -3,9 +3,9 @@
 SENDDIO=${SENDDIO-../../../programs/senddio/senddio}
 set -e
 
-#Usage: senddio [--verbose] 
+#Usage: senddio [--verbose]
 #               [--prefix prefix]     -p      2001:db8:0001::/48
-#               [-d datafile] 
+#               [-d datafile]
 #               [--prefixlifetime]    -P       12
 #               [--fake]              -T      YES
 #               [--iface net]         -i
@@ -27,7 +27,8 @@ rm -f $out
 ${SENDDIO} --pcapout $out --fake -i wlan0 -v \
     --dagid T1 --prefix 2001:db8:0001::/48 --prefixlifetime 12 \
     --instance 42 \
-    --grounded --storing --version 1 --sequence 10 --rank 2 
-) | tee ../OUTPUTS/senddio-test-03.raw | diff - senddio-test-03.out
+    --grounded --storing --version 1 --sequence 10 --rank 2
+) | tee ../OUTPUTS/senddio-test-03.raw | diff -B -w - senddio-test-03.out
+hexdump -C ../OUTPUTS/senddio-test-03.raw senddio-test-03.out
 
-tcpdump -t -n -r $out -v -X | tee ../OUTPUTS/senddio-test-03-pcap.raw | diff - senddio-test-03-pcap.txt
+tcpdump -t -n -r $out -v -X | tee ../OUTPUTS/senddio-test-03-pcap.raw | diff -B -w - senddio-test-03-pcap.txt
