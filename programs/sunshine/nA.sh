ebe5635515d5dc8755b9fe88e1af36b45fdc534e
Michael Richardson <mcr@sandelman.ca>
2013-12-31 14:11:12 -0500

    scripts to start daemons

--- programs/sunshine/nA.sh
+++ programs/sunshine/nA.sh
@@ -2,4 +2,5 @@
 
 /sbin/sunshine -K
 sleep 2
-/sbin/sunshine -v --dagid FUNFUNFUN -i eth1 -W 10000 --prefix 2001:1234:5678::/48 --rank 1 -I 1
+/sbin/sunshine -v --dagid FUNFUNFUN -i eth1 -W 10000 --prefix 2001:1234:5678::/48 --rank 1 -I 1 2>&1 | tee -a /var/log/nA.log
+
--- programs/sunshine/nE.sh
+++ programs/sunshine/nE.sh
@@ -2,4 +2,4 @@
 
 /sbin/sunshine -K
 sleep 2
-/sbin/sunshine -v --dagid FUNFUNFUN -W 10000 -i eth0 -i eth1
+/sbin/sunshine -v --dagid FUNFUNFUN -W 10000 -i eth0 -i eth1 2>&1 | tee -a /var/log/nE.log
