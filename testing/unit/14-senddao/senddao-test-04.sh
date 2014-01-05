e3330fe872bb5f8102028d2248a33f6c81471a1e
Michael Richardson <mcr@sandelman.ca>
2014-01-05 12:04:01 -0500

    updates to SENDDAO test: include acK flag, tcpdump the final packet

--- testing/unit/14-senddao/senddao-test-04.sh
+++ testing/unit/14-senddao/senddao-test-04.sh
@@ -6,5 +6,5 @@ PCAP04=../OUTPUTS/senddao-test-04out.pcap
 ${SENDDAO} --fake --outpcap ${PCAP04} -i wlan0 -v --dagid thisismydicedag2 --sequence 11 --instance 43 --prefix fdfd:abcd:ef01::/48 2>&1 |
   tee ../OUTPUTS/senddao-test-04.raw | diff -B -w - senddao-test-04.out
 
-# disabled #2901
-#tcpdump -t -n -r ${PCAP04} -v -v | tee ../OUTPUTS/senddao-test-04-pcap.txt | diff - senddao-test-04-pcap.txt
+tcpdump -t -n -r ${PCAP04} -v -v | tee ../OUTPUTS/senddao-test-04-pcap.txt | diff - senddao-test-04-pcap.txt
+
