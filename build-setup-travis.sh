d0a693372d6d030be118f62edc811b64b06d1b7f
Michael Richardson <mcr@sandelman.ca>
2014-09-28 21:40:25 -0400

    move travis setup instructions to a seperate file so that they can be re-used

---
+++ build-setup-travis.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+(cd .. && curl http://www.ca.tcpdump.org/release/libpcap-1.6.2.tar.gz | tar xzf - )
+(cd .. && curl http://www.ca.tcpdump.org/release/tcpdump-4.6.2.tar.gz | tar xzf - )
+(cd .. && mkdir -p host/libpcap-1.6.2 && cd host/libpcap-1.6.2 && ../../libpcap-1.6.2/configure && make )
+(cd .. && mkdir -p host/tcpdump-4.6.2 && cd host/tcpdump-4.6.2 && ../../tcpdump-4.6.2/configure && make )
+(cd .. && mkdir -p x86/libpcap-1.6.2 && cd x86/libpcap-1.6.2 && CFLAGS=-m32 ../../libpcap-1.6.2/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+(cd .. && ln -s x86/libpcap-1.6.2 libpcap && mkdir -p x86/tcpdump-4.6.2 && cd x86/tcpdump-4.6.2 && CFLAGS=-m32 ../../tcpdump-4.6.2/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+echo LIBPCAP=$(pwd)/../x86/libpcap-1.6.2/libpcap.a >Makefile.local
+echo TCPDUMP=$(pwd)/../host/tcpdump-4.6.2/tcpdump  >>Makefile.local
+echo NETDISSECTLIB=$(pwd)/../host/tcpdump-4.6.2/libnetdissect.a >>Makefile.local
+echo NETDISSECTH=-DNETDISSECT -I$(pwd)/../host/tcpdump-4.6.2/ -I$(pwd)/../tcpdump-4.6.2 >>Makefile.local
