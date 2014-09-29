2b0e5cb87a53c13f63f67070ac5551d286d1c79a
Michael Richardson <mcr@sandelman.ca>
2014-09-28 23:03:43 -0400

    make sure to be able to find installed pcap headers

--- build-setup-travis.sh
+++ build-setup-travis.sh
@@ -8,6 +8,7 @@ set -e
 (cd .. && mkdir -p x86/libpcap-1.6.2 && cd x86/libpcap-1.6.2 && CFLAGS=-m32 ../../libpcap-1.6.2/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
 (cd .. && ln -s x86/libpcap-1.6.2 libpcap && mkdir -p x86/tcpdump-4.6.2 && cd x86/tcpdump-4.6.2 && CFLAGS=-m32 ../../tcpdump-4.6.2/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
 echo LIBPCAP=$(pwd)/../x86/libpcap-1.6.2/libpcap.a >Makefile.local
+echo LIBPCAPINC=-I$(pwd)/../include                >>Makefile.local
 echo TCPDUMP=$(pwd)/../host/tcpdump-4.6.2/tcpdump  >>Makefile.local
 echo NETDISSECTLIB=$(pwd)/../host/tcpdump-4.6.2/libnetdissect.a >>Makefile.local
 echo NETDISSECTH=-DNETDISSECT -I$(pwd)/../include -I$(pwd)/../host/tcpdump-4.6.2/ -I$(pwd)/../tcpdump-4.6.2 >>Makefile.local
