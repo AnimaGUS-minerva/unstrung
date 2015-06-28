652d270c76a9380444e1eef2bc652c080b70d73a
Michael Richardson <mcr@sandelman.ca>
2015-06-27 21:24:30 -0400

    released libpcap 1.7.4 to fix out-of-directory build and use it

--- build-setup-travis.sh
+++ build-setup-travis.sh
@@ -2,14 +2,14 @@
 
 set -e
 BUILDTOP=$(cd ..; pwd)
-(cd .. && curl http://www.ca.tcpdump.org/release/libpcap-1.7.3.tar.gz | tar xzf - )
+(cd .. && curl http://www.ca.tcpdump.org/release/libpcap-1.7.4.tar.gz | tar xzf - )
 (cd .. && curl http://www.ca.tcpdump.org/release/tcpdump-4.7.4.tar.gz | tar xzf - )
-(cd .. && mkdir -p host/libpcap-1.7.3 && cd host/libpcap-1.7.3 && ../../libpcap-1.7.3/configure --prefix=${BUILDTOP} && make && make install)
+(cd .. && mkdir -p host/libpcap-1.7.4 && cd host/libpcap-1.7.4 && ../../libpcap-1.7.4/configure --prefix=${BUILDTOP} && make && make install)
 (cd .. && mkdir -p host/tcpdump-4.7.4 && cd host/tcpdump-4.7.4 && ../../tcpdump-4.7.4/configure --prefix=${BUILDTOP} && make && make install)
-(cd .. && mkdir -p x86/libpcap-1.7.3 && cd x86/libpcap-1.7.3 && CFLAGS=-m32 ../../libpcap-1.7.3/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
-(cd .. && ln -s x86/libpcap-1.7.3 libpcap && mkdir -p x86/tcpdump-4.7.4 && cd x86/tcpdump-4.7.4 && CFLAGS=-m32 ../../tcpdump-4.7.4/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
-echo LIBPCAP=${BUILDTOP}/x86/libpcap-1.7.3/libpcap.a >Makefile.local
-echo LIBPCAP_HOST=${BUILDTOP}/host/libpcap-1.7.3/libpcap.a >>Makefile.local
+(cd .. && mkdir -p x86/libpcap-1.7.4 && cd x86/libpcap-1.7.4 && CFLAGS=-m32 ../../libpcap-1.7.4/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+(cd .. && ln -s x86/libpcap-1.7.4 libpcap && mkdir -p x86/tcpdump-4.7.4 && cd x86/tcpdump-4.7.4 && CFLAGS=-m32 ../../tcpdump-4.7.4/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+echo LIBPCAP=${BUILDTOP}/x86/libpcap-1.7.4/libpcap.a >Makefile.local
+echo LIBPCAP_HOST=${BUILDTOP}/host/libpcap-1.7.4/libpcap.a >>Makefile.local
 echo LIBPCAPINC=-I${BUILDTOP}/include                >>Makefile.local
 echo ARCH=i386  			             >>Makefile.local
 echo TCPDUMP=${BUILDTOP}/host/tcpdump-4.7.4/tcpdump  >>Makefile.local
