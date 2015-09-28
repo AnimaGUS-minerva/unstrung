23b2c40a07cc50090d5fc269aaee4f1ab95d97e6
Michael Richardson <mcr@sandelman.ca>
2015-09-28 14:53:11 -0400

    added test case 29: ignore PIO

---
+++ testing/unit/29-daoreply-nopio/29-daoreply-nopio.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "iface.h"
+
+#include "fakeiface.h"
+
+/* refactored because test case 27 uses this */
+#include "../24-daoreply/dioA.cpp"
+#include "../24-daoreply/daoackA.cpp"
+
+int main(int argc, char *argv[])
+{
+        rpl_debug *deb = new rpl_debug(true, stdout);
+        deb->want_time_log = false;
+
+        dag_network *dag = dioA_setup(deb);
+
+        dioA_process(dag, deb, "../OUTPUTS/24-node-E-out.pcap");
+        daoackA_step(deb, "../INPUTS/daoack-A-ripple1.pcap", NULL);
+
+	exit(0);
+}
+
+
