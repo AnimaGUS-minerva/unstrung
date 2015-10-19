d80489a0afbe0d11f1121f6625de5ebb4c700302
Michael Richardson <mcr@sandelman.ca>
2015-10-19 14:10:37 -0400

    instructions on building for openwrt

---
+++ packaging/openwrt/build-unstrung.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+make package/unstrung/prepare USE_SOURCE_DIR=/ssw/projects/pandora/unstrung V=s
+make package/unstrung/compile USE_SOURCE_DIR=/ssw/projects/pandora/unstrung V=s
---
+++ packaging/openwrt/upload.sh
@@ -0,0 +1,1 @@
+cp bin/ramips/packages/base/unstrung_1.00-1_ramips_24kec.ipk /ssw/projects/bluerose/sites/unstrung.sandelman.ca/downloads/ipkg
