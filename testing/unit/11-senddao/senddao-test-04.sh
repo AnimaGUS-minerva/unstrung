ed5bff90781f88fd49296acd5828e686813237be
Michael Richardson <mcr@sandelman.ca>
2016-06-22 18:14:08 -0400

    change from symbolic dagid to IPv6

--- testing/unit/11-senddao/senddao-test-04.sh
+++ testing/unit/11-senddao/senddao-test-04.sh
@@ -3,7 +3,7 @@
 SENDDAO=${SENDDAO-../../../programs/senddao/senddao}
 PCAP04=../OUTPUTS/senddao-test-04out.pcap
 
-${SENDDAO} -v -v --fake --outpcap ${PCAP04} -i wlan0 -v  --instance 2 --dagid T1 --sequence 1 --prefix 2001:db8::/38 --myid 2001:db8::abcd:1 --dest fe80::1200:00ff:fe64:6423 --target 2001:db8::abcd:2/128  2>&1 |
+${SENDDAO} -v -v --fake --outpcap ${PCAP04} -i wlan0 -v  --instance 2 --dagid fd80:abcd::12 --sequence 1 --prefix 2001:db8::/38 --myid 2001:db8::abcd:1 --dest fe80::1200:00ff:fe64:6423 --target 2001:db8::abcd:2/128  2>&1 |
   tee ../OUTPUTS/senddao-test-04.raw | diff -B -w - senddao-test-04.out
 
 ${TCPDUMP-tcpdump} -t -n -r ${PCAP04} -v -v -v | tee ../OUTPUTS/senddao-test-04-pcap.txt | diff - senddao-test-04-pcap.txt
