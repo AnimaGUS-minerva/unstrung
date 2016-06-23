3c3bbc45801cee1b871059943318069c6985a14d
Michael Richardson <mcr@sandelman.ca>
2016-06-23 09:36:17 -0400

    change dagid to v6

--- testing/unit/12-senddio/senddio-test-04.sh
+++ testing/unit/12-senddio/senddio-test-04.sh
@@ -20,14 +20,14 @@ set -e
 #               [--rank #]            -R        2
 #               [--dagid hexstring]   -D        thisismynicedag1
 
-
+#set -x
 (
-${SENDDIO} --fake -i wlan0 -v -I 42 -D thisismynicedag1 -p 2001:db8:0001::/48 -G --version 1 -S 10 -R 2 -s  -P 12
-) | tee ../OUTPUTS/senddio-test-04.raw | diff -w -B - senddio-test-04.out
+${SENDDIO} --fake -i wlan0 -v -I 42 --dagid 2001:db8:1::1 -p 2001:db8:0001::/48 -G --version 1 -S 10 -R 2 -s  -P 12
+) | tee ../OUTPUTS/senddio-test-04a.raw | diff -w -B - senddio-test-04.out
 
 (
 ${SENDDIO} --fake -i wlan0 -v \
            --instance 42 \
-           --dagid thisismynicedag1 --prefix 2001:db8:0001::/48 --prefixlifetime 12 \
+           --dagid 2001:db8:1::1 --prefix 2001:db8:0001::/48 --prefixlifetime 12 \
            --grounded --storing --version 1 --sequence 10 --rank 2
-) | tee ../OUTPUTS/senddio-test-04.raw | diff -w -B - senddio-test-04.out
+) | tee ../OUTPUTS/senddio-test-04b.raw | diff -w -B - senddio-test-04.out
