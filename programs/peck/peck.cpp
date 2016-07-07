c2951377bce44677cfffea446a41f80bb13e9940
Michael Richardson <mcr@sandelman.ca>
2016-07-06 22:17:48 -0400

    added --fake and list of interfaces

--- programs/peck/peck.cpp
+++ programs/peck/peck.cpp
@@ -22,18 +22,33 @@ static void usage(void)
 int main(int argc, char *argv[])
 {
   int verbose = 0;
+  bool fakesend=false;
+  bool initted =false;
   char c;
+  char *micpemfile;
+  char *micprivfile;
   class rpl_debug *deb;
   struct option longoptions[]={
-    {"version",  1, NULL, 'V'},
+    {"help",     0, NULL, '?'},
+    {"fake",     0, NULL, 'T'},
+    {"mic",      1, NULL, 'm'},
+    {"privmic",  1, NULL, 'M'},
     {"verbose",  0, NULL, 'v'},
     {0,0,0,0},
   };
 
   deb = new rpl_debug(verbose, stderr);
 
-  while((c=getopt_long(argc, argv, "?hvV", longoptions, NULL))!=EOF){
+  while((c=getopt_long(argc, argv, "?hm:vFM:V", longoptions, NULL))!=EOF){
     switch(c) {
+    case 'T':
+      if(initted) {
+        fprintf(stderr, "--fake MUST be first argument\n");
+        exit(16);
+      }
+      fakesend=true;
+      break;
+
     case 'v':
       verbose++;
       deb->set_verbose(stderr);
@@ -44,6 +59,14 @@ int main(int argc, char *argv[])
       usage();
       break;
 
+    case 'm':
+      micpemfile = strdup(optarg);
+      break;
+
+    case 'M':
+      micprivfile= strdup(optarg);
+      break;
+
     case '?':
     case 'h':
     default:
@@ -51,7 +74,24 @@ int main(int argc, char *argv[])
       break;
 
     }
+    initted = true;
   }
+
+  if(!initted) {
+    if(fakesend) {
+      pcap_network_interface::scan_devices(deb, false);
+    } else {
+      network_interface::scan_devices(deb, false);
+    }
+  }
+
+  for(int ifnum = optind; ifnum < argc; ifnum++) {
+    class pcap_network_interface *iface = NULL;
+    iface = (pcap_network_interface *)pcap_network_interface::find_by_name(argv[ifnum]);
+
+    printf("working on interface: %s\n", argv[ifnum]);
+  }
+
 
   exit(0);
 }
