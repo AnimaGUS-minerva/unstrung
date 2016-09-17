56cb853490ab99bfe2d701d02c6f6f28545b961e
Michael Richardson <mcr@credil.org>
2016-09-16 22:11:12 -0400

    added devid.h for neighbourdiscovery work

---
+++ include/devid.h
@@ -0,0 +1,43 @@
+#ifndef _UNSTRUNG_DEVID_H_
+#define _UNSTRUNG_DEVID_H_
+
+#include <map>
+
+extern "C" {
+#include <errno.h>
+#include <signal.h>
+#include <pathnames.h>		/* for PATH_PROC_NET_IF_INET6 */
+#include <arpa/inet.h>
+#include <netinet/ip6.h>
+#include <sys/time.h>
+#include <netinet/icmp6.h>
+#include <linux/if.h>             /* for IFNAMSIZ */
+#include "oswlibs.h"
+}
+
+#define HWADDR_MAX 16
+#include "unstrung.h"
+#include "iface.h"
+
+class device_identity {
+public:
+  int build_neighbour_advert(network_interface *iface,
+                             unsigned char *buff,
+                             unsigned int buff_len);
+
+protected:
+  int foo;
+
+private:
+  int bar;
+};
+
+#endif /* _UNSTRUNG_DEVID_H_ */
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: whitesmith
+ * End:
+ */
+
