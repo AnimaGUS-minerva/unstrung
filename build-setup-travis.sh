8e0b28a58dc185da095ea31aa29f42bcdca4f72e
Michael Richardson <mcr@sandelman.ca>
2016-10-09 23:03:17 -0400

    move apt-list to apt-dependancies, and restructure contents of ~/stuff

--- build-setup-travis.sh
+++ build-setup-travis.sh
@@ -5,38 +5,50 @@ mkdir -p $HOME/stuff
 BUILDTOP=$(cd $HOME/stuff; pwd)
 ARCH=$(arch)
 
+case $ARCH in
+    x86_64) HOST=x86_64;;
+    i386)   HOST=i386;;
+esac
+
+set -x
+rm -f ${BUILDTOP}/host
+ln -f -s ${BUILDTOP}/${HOST} ${BUILDTOP}/host
+mkdir -p ${BUILDTOP}/${HOST}
+
 if [ ! -d $BUILDTOP/include/mbedtls ]; then
     if [ ! -d $BUILDTOP/mbedtls ]; then (cd ${BUILDTOP} && git clone -b mcr_add_otherName https://github.com/mcr/mbedtls.git ); fi
+    set -x
     (cd ${BUILDTOP} && rm -rf host/mbedtls && mkdir -p host/mbedtls && cd host/mbedtls && cmake -DCMAKE_INSTALL_PREFIX=$BUILDTOP ../../mbedtls && make && make install)
 fi
 
 LIBPCAP=${BUILDTOP}/host/libpcap-1.8.0/libpcap.a
 if [ ! -x $BUILDTOP/host/tcpdump-4.8.0/tcpdump ]
 then
-    if [ ! -d libpcap ]; then (cd ${BUILDTOP} && git clone https://github.com/mcr/libpcap.git ); fi
-    if [ ! -d tcpdump ]; then (cd ${BUILDTOP} && git clone https://github.com/mcr/tcpdump.git ); fi
+    if [ ! -d ${BUILDTOP}/libpcap ]; then (cd ${BUILDTOP} && git clone https://github.com/mcr/libpcap.git ); fi
+    if [ ! -d ${BUILDTOP}/tcpdump ]; then (cd ${BUILDTOP} && git clone https://github.com/mcr/tcpdump.git ); fi
     case $ARCH in
         x86_64) HOST=x86_64;;
         i386)   HOST=i386;;
     esac
 
-    (cd ${BUILDTOP} && mkdir -p ${HOST}/libpcap-1.8.0 && ln -s ${HOST} host && cd ${HOST}/libpcap-1.8.0 && ../../libpcap/configure --prefix=${BUILDTOP} && make && make install)
-    (cd ${BUILDTOP} && mkdir -p ${HOST}/tcpdump-4.8.0 && ln -s ${HOST} host && cd ${HOST}/tcpdump-4.8.0 && ../../tcpdump/configure --prefix=${BUILDTOP} && make && make install)
-
-    if [ ! -d "x86_64" ]; then
-        (cd ${BUILDTOP} && mkdir -p x86/libpcap-1.8.0 && cd x86/libpcap-1.8.0 && CFLAGS=-m64 ../../libpcap/configure --target=x86_64-linux-gnu && make LDFLAGS=-m64 CFLAGS=-m64)
-        (cd ${BUILDTOP} && ln -s x86/libpcap-1.8.0 libpcap && mkdir -p x86/tcpdump-4.8.0 && cd x86/tcpdump-4.8.0 && CFLAGS=-m64 ../../libpcap/configure --target=x86_64-linux-gnu && make LDFLAGS=-m64 CFLAGS=-m64)
+    if [ ! -d "${BUILDTOP}/x86_64/tcpdump/." ]; then
+        (cd ${BUILDTOP} && mkdir -p x86_64/libpcap-1.8.0 && cd x86_64/libpcap-1.8.0 && CFLAGS=-m64 ../../libpcap/configure --prefix=$HOME/stuff --target=x86_64-linux-gnu && make LDFLAGS=-m64 CFLAGS=-m64)
+        (cd ${BUILDTOP} && ln -f -s x86_64/libpcap-1.8.0 libpcap && mkdir -p x86_64/tcpdump-4.8.0 && cd x86_64/tcpdump-4.8.0 && CFLAGS=-m64 ../../tcpdump/configure --prefix=$HOME/stuff --target=x86_64-linux-gnu && make LDFLAGS=-m64 CFLAGS=-m64)
+        (cd ${BUILDTOP}/x86_64 && ln -s -f tcpdump-4.8.0 tcpdump )
     fi
 
-    if [ ! -d "i386" ]; then
-        (cd ${BUILDTOP} && mkdir -p i386/libpcap-1.8.0 && cd i386/libpcap-1.8.0 && CFLAGS=-m64 ../../libpcap/configure --target=x86_64-linux-gnu && make LDFLAGS=-m64 CFLAGS=-m64)
-        (cd ${BUILDTOP} && ln -s i386/libpcap-1.8.0 libpcap && mkdir -p i386/tcpdump-4.8.0 && cd i386/tcpdump-4.8.0 && CFLAGS=-m32 ../../tcpdump/configure --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+    if [ ! -d "${BUILDTOP}/i386/tcpdump/." ]; then
+        (cd ${BUILDTOP} && mkdir -p i386/libpcap-1.8.0 && cd i386/libpcap-1.8.0 && CFLAGS=-m32 ../../libpcap/configure --prefix=$HOME/stuff --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+        (cd ${BUILDTOP} && ln -f -s i386/libpcap-1.8.0 libpcap && mkdir -p i386/tcpdump-4.8.0 && cd i386/tcpdump-4.8.0 && CFLAGS=-m32 ../../tcpdump/configure --prefix=$HOME/stuff --target=i686-pc-linux-gnu && make LDFLAGS=-m32 CFLAGS=-m32)
+        (cd ${BUILDTOP}/i386 && ln -s -f tcpdump-4.8.0 tcpdump )
     fi
+
+    (cd $BUILDTOP/host/libpcap-1.8.0 && make install)
 fi
 
 if [ ! -f $BUILDTOP/include/cobr.h ]
 then
-    if [ ! -d libcbor ]; then git clone https://github.com/mcr/libcbor.git ; fi
+    if [ ! -d ${BUILDTOP}/libcbor ]; then ( cd $BUILDTOP && git clone https://github.com/mcr/libcbor.git) ; fi
 
     case $ARCH in
         x86_64) HOST=x86_64;;
@@ -47,11 +59,11 @@ then
 
 
 
-    if [ ! -d "x86_64/libcbor" ]; then
+    if [ ! -d "${BUILDTOP}/x86_64/libcbor" ]; then
         (cd ${BUILDTOP} && mkdir -p x86_64/libcbor && cd x86_64/libcbor && cmake ../../libcbor -DCMAKE_C_FLAGS_DEBUG=-m64 -DCMAKE_INSTALL_PREFIX:PATH=${BUILDTOP}/x86_64 && make && make install)
     fi
 
-    if [ ! -d "i386/libcbor" ]; then
+    if [ ! -d "${BUILDTOP}/i386/libcbor" ]; then
         (cd ${BUILDTOP} && mkdir -p i386/libcbor && cd i386/libcbor && cmake ../../libcbor -DCMAKE_C_FLAGS_DEBUG=-m32 -DCMAKE_INSTALL_PREFIX:PATH=${BUILDTOP}/i386 && make && make install)
     fi
 fi
@@ -60,12 +72,12 @@ LIBPCAP=${BUILDTOP}/host/libpcap-1.8.0/libpcap.a
 
 echo LIBPCAP=${LIBPCAP} -lpthread -ldbus-1 >Makefile.local
 echo LIBPCAP_HOST=${BUILDTOP}/host/libpcap-1.8.0/libpcap.a -lpthread -lusb-1.0 -ldbus-1 >>Makefile.local
-echo LIBPCAPINC=-I${BUILDTOP}/include                >>Makefile.local
+echo LIBPCAPINC=-I${BUILDTOP}/host/libpcap-1.8.0     >>Makefile.local
 echo TCPDUMP=${BUILDTOP}/host/tcpdump-4.8.0/tcpdump  >>Makefile.local
 echo MBEDTLSH=-I${BUILDTOP}/include                  >>Makefile.local
 echo MBEDTLSLIB=${BUILDTOP}/lib                      >>Makefile.local
 echo NETDISSECTLIB=${BUILDTOP}/host/tcpdump-4.8.0/libnetdissect.a >>Makefile.local
 echo NETDISSECTH=-DNETDISSECT -I${BUILDTOP}/include -I${BUILDTOP}/host/tcpdump-4.8.0/ -I${BUILDTOP}/tcpdump >>Makefile.local
-echo CBOR_LIB=${BUILDTOP}/${HOST}/lib/libcbor.a      >>Makefile.local
+echo CBOR_LIB=${BUILDTOP}/${HOST}/libcbor/src/libcbor.a      >>Makefile.local
 echo CBOR_INCLUDE=-I${BUILDTOP}/include -Drestrict=__restrict__   >>Makefile.local
 echo export ARCH LIBPCAP LIBPCAP_HOST LIBPCAPINC TCPDUMP NETDISSECTLIB NETDISSECTH >>Makefile.local
---
+++ install-apt-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apt-get install dpkg-dev gcc-multilib g++-multilib pkg-config libssl1.0.0 libssl-dev
+#cp /usr/lib/x86_64-linux-gnu/libcrypto* /tmp
+apt-get install build-essential lib32stdc++6 libdbus-1-dev libusb-1.0-0-dev:i386 libusb-1.0-0-dev:amd64 lib32z1-dev flex bison libssl-dev:i386 libboost-dev libcmocka-dev libcmocka0:i386 libcmocka0:amd64 cmake
+sudo apt-get install ruby1.9.1-dev
+#mv /tmp/libcrypto* /usr/lib/x86_64-linux-gnu
+gem install ronn cbor cbor-diag
+
