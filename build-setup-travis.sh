a0e0f0151eb7e7a7d3c34a189b5ed68c7fb204a7
Michael Richardson <mcr@sandelman.ca>
2016-10-27 13:04:01 -0400

    put all of the versioned directories into a single variable

--- build-setup-travis.sh
+++ build-setup-travis.sh
@@ -28,6 +28,7 @@ then
     if [ ! -d ${BUILDTOP}/tcpdump ]; then (cd ${BUILDTOP} && git clone -b tcpdump-4.8.1 https://github.com/the-tcpdump-group/tcpdump.git ); fi
     case $ARCH in
         x86_64) HOST=x86_64;;
+
         i386)   HOST=i386;;
     esac
 
@@ -45,6 +46,10 @@ then
 
     (cd $BUILDTOP/host/libpcap-1.8.1 && make install)
 fi
+LIBPCAP_HOST_DIR=${BUILDTOP}/${HOST}/libpcap-1.8.1
+LIBPCAP_HOST=${LIBPCAP}/libpcap.a
+LIBPCAP=${LIBPCAP_HOST}
+TCPDUMP_HOST_DIR=${BUILDTOP}/${HOST}/tcpdump-4.8.1
 
 if [ ! -f $BUILDTOP/include/cobr.h ]
 then
@@ -68,16 +73,14 @@ then
     fi
 fi
 
-LIBPCAP=${BUILDTOP}/host/libpcap-1.8.0/libpcap.a
-
 echo LIBPCAP=${LIBPCAP} -lpthread -ldbus-1 >Makefile.local
-echo LIBPCAP_HOST=${BUILDTOP}/host/libpcap-1.8.0/libpcap.a -lpthread -lusb-1.0 -ldbus-1 >>Makefile.local
-echo LIBPCAPINC=-I${BUILDTOP}/host/libpcap-1.8.0     >>Makefile.local
-echo TCPDUMP=${BUILDTOP}/host/tcpdump-4.8.0/tcpdump  >>Makefile.local
+echo LIBPCAP_HOST=${LIBPCAP_HOST} -lpthread -lusb-1.0 -ldbus-1 >>Makefile.local
+echo LIBPCAPINC=-I${LIBPCAP_HOST_DIR}                >>Makefile.local
+echo TCPDUMP=${TCPDUMP_HOST_DIR}/tcpdump             >>Makefile.local
 echo MBEDTLSH=-I${BUILDTOP}/include                  >>Makefile.local
 echo MBEDTLSLIB=${BUILDTOP}/lib                      >>Makefile.local
-echo NETDISSECTLIB=${BUILDTOP}/host/tcpdump-4.8.0/libnetdissect.a >>Makefile.local
-echo NETDISSECTH=-DNETDISSECT -I${BUILDTOP}/include -I${BUILDTOP}/host/tcpdump-4.8.0/ -I${BUILDTOP}/tcpdump >>Makefile.local
+echo NETDISSECTLIB=${LIBPCAP_HOST_DIR}/libnetdissect.a >>Makefile.local
+echo NETDISSECTH=-DNETDISSECT -I${BUILDTOP}/include -I${TCPDUMP_HOST_DIR} -I${BUILDTOP}/tcpdump >>Makefile.local
 echo CBOR_LIB=${BUILDTOP}/${HOST}/libcbor/src/libcbor.a      >>Makefile.local
 echo CBOR_INCLUDE=-I${BUILDTOP}/include -Drestrict=__restrict__   >>Makefile.local
 echo export ARCH LIBPCAP LIBPCAP_HOST LIBPCAPINC TCPDUMP NETDISSECTLIB NETDISSECTH >>Makefile.local
