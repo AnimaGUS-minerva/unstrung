6c149dcf6b15041af1d867ff166e32b497d735e6
Michael Richardson <mcr@sandelman.ca>
2016-11-01 22:27:46 -0400

    configure kvm network interfaces for A, E, J

--- testing/baseconfigs/net.A.sh
+++ testing/baseconfigs/net.A.sh
@@ -5,7 +5,8 @@ mac_eth0=12:00:00:dc:bc:ff
 if [ -n "$UML_ground_CTL" ]
 then
     net_eth0="eth0=daemon,$mac_eth0,unix,$UML_ground_CTL";
-    kvm_eth0="-net nic,vlan=0,name=Aground,macaddr=$mac_eth0,model=virtio"
+    kvm_eth0="-net nic,vlan=0,model=virtio,name=Aground,macaddr=$mac_eth0 "
+    kvm_eth0="$kvm_eth0 -net vde,vlan=0,sock=${VDE_DIR}/0"
 else
     mcast_eth0=239.192.0.1,21200
     net_eth0="eth0=mcast,$mac_eth0,$mcast_eth0"
@@ -16,7 +17,8 @@ mac_eth1=12:00:00:64:64:23
 if [ -n "$UML_n1_CTL" ]
 then
     net_eth1="eth1=daemon,$mac_eth1,unix,$UML_n1_CTL";
-    kvm_eth1="-net nic,vlan=1,name=An1,macaddr=$mac_eth1,model=virtio"
+    kvm_eth1="-net nic,vlan=1,model=virtio,name=An1,macaddr=$mac_eth1 "
+    kvm_eth1="$kvm_eth1 -net vde,vlan=1,sock=${VDE_DIR}/1"
 else
     net_eth1="eth1=mcast,$mac_eth1,239.192.1.2,31200";
     kvm_eth1="-net nic,vlan=1,name=An1,macaddr=$mac_eth1,model=virtio -net socket,vlan=1,mcast=239.192.1.2:31200"
@@ -24,15 +26,15 @@ fi
 
 mac_eth2=12:00:00:64:64:24
 net_eth2=""
+kvm_eth2=""
 if [ -r /var/run/uml-utilities/uml_switch.ctl ];
 then
     net_eth2="eth2=daemon,$mac_eth2,unix,/var/run/uml-utilities/uml_switch.ctl";
 fi
-kvm_eth2
 if [ -d /tmp/vde.ctl ];
 then
-    kvm_eth2="-net nic,vlan=2,model=virtio,name=Aobiwan,macaddr=$mac_eth2 "
-    kvm_eth2="$kvm_eth2 -net vde,vlan=2,sock=/tmp/vde.ctl";
+    kvm_eth2="-net nic,vlan=99,model=virtio,name=Aobiwan,macaddr=$mac_eth2 "
+    kvm_eth2="$kvm_eth2 -net vde,vlan=99,sock=${VDE_DIR}/99";
 fi
 
 net_slip="ssl0=port:21201"
--- testing/baseconfigs/net.E.sh
+++ testing/baseconfigs/net.E.sh
@@ -1,21 +1,37 @@
 #
-# network for E, between n1 and n2. (parallel to B)
+# network for E, between n1 and n2.
 #
+mac_eth0=12:00:00:66:69:01
 if [ -n "$UML_n2_CTL" ]
 then
-    net_eth0="eth0=daemon,12:00:00:66:66:01,unix,$UML_n2_CTL";
+    net_eth0="eth0=daemon,$mac_eth0,unix,$UML_n2_CTL";
+    kvm_eth0="-net nic,vlan=1,model=virtio,name=En2,macaddr=$mac_eth0 "
+    kvm_eth0="$kvm_eth0 -net vde,vlan=2,sock=${VDE_DIR}/2";
 else
-    net_eth0="eth0=mcast,12:00:00:66:66:01,239.192.0.1,21300"
+    net_eth0="eth0=mcast,12:00:00:66:69:01,239.192.0.1,21300"
 fi
 
+mac_eth1=12:00:00:66:69:02
 if [ -n "$UML_n1_CTL" ]
 then
-    net_eth1="eth1=daemon,12:00:00:66:66:02,unix,$UML_n1_CTL";
+    net_eth1="eth1=daemon,$mac_eth1,unix,$UML_n1_CTL";
+    kvm_eth1="-net nic,vlan=1,model=virtio,name=En1,macaddr=$mac_eth1 "
+    kvm_eth1="$kvm_eth1 -net vde,vlan=1,sock=${VDE_DIR}/1";
 else
-    net_eth1="eth1=mcast,12:00:00:66:66:02,239.192.1.2,31200";
+    net_eth1="eth1=mcast,12:00:00:66:69:02,239.192.1.2,31200";
+fi
+
+mac_eth2=12:00:00:66:69:03
+if [ -d /tmp/vde.ctl ];
+then
+    kvm_eth2="-net nic,vlan=99,model=virtio,name=Eobiwan,macaddr=$mac_eth2 "
+    kvm_eth2="$kvm_eth2 -net vde,vlan=99,sock=${VDE_DIR}/99";
 fi
 
 net="$net_eth0 $net_eth1"
+kvmnet="$kvm_eth0 $kvm_eth1 $kvm_eth2"
+
+
 
 
 
