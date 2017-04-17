da1016c7c8437bdf18ef98df79b2f4c933bda841
Michael Richardson <mcr@sandelman.ca>
2017-04-17 14:34:06 -0400

    updated Makefile.local

--- install-git-deps.sh
+++ install-git-deps.sh
@@ -7,5 +7,12 @@ cd $BUILDTOP
 [ -d mbedtls ] || git clone -b mcr_add_otherName https://github.com/mcr/mbedtls.git
 (cd mbedtls && cmake -DCMAKE_INSTALL_PREFIX=$BUILDTOP . && make && make install )
 
+touch Makefile.local
+if ! grep MBEDTLS Makefile.local
+then
+    echo MBEDTLSH=-I${BUILDTOP}/include          >>Makefile.local
+    echo MBEDTLSLIB=${BUILDTOP}/lib              >>Makefile.local
+fi
+
 
 
