0e231c9b6aa744c4685b0888ddffe669742b21d4
Michael Richardson <mcr@sandelman.ca>
2017-04-18 21:09:02 +0000

    some tweaks to the list of apt-dependancies, and the Makefile inclusion of variables now includes an explicit space in case some variables are nil

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -7,7 +7,9 @@ fi
 sudo apt-get install dpkg-dev pkg-config libssl1.0.0 libssl-dev
 #cp /usr/lib/x86_64-linux-gnu/libcrypto* /tmp
 
-sudo apt-get install build-essential libdbus-1-dev libusb-1.0-0-dev flex bison libboost-dev libcmocka-dev libcmocka0 cmake libnl-3-200 libnl-genl-3-200 libnl-3-dev libnl-genl-3-dev libusb-1.0-0
+sudo apt-get install build-essential libdbus-1-dev libusb-1.0-0-dev flex bison libboost-dev libcmocka-dev libcmocka0 \
+     cmake libnl-3-200 libnl-genl-3-200 libnl-3-dev libnl-genl-3-dev libusb-1.0-0 \
+     libpcap-dev
 
 
 
