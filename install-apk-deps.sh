ca305ff877259a165f6d5d476a8a8b6d626cb4e7
Michael Richardson <mcr@sandelman.ca>
2021-04-23 23:41:16 -0400

    additional installation instructions

---
+++ install-apk-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# these are dependancies for ALPINE Linux (not Android)
+
+sudo apk update
+
+sudo apk install flex bison boost-dev cmake libpcap-dev cmocka-dev
+
+# libdbus-1-dev libusb-1.0-0-dev libcmocka-dev libcmocka0
+# libnl-3-200 libnl-genl-3-200 libnl-3-dev libnl-genl-3-dev libusb-1.0-0
+
+
+
