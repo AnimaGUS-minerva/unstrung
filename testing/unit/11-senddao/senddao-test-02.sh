384aac90f7b81784a44637087beb1674a7339a10
Michael Richardson <mcr@sandelman.ca>
2021-04-24 19:37:45 -0400

    write gdb instructions to file

--- testing/unit/11-senddao/senddao-test-02.sh
+++ testing/unit/11-senddao/senddao-test-02.sh
@@ -6,6 +6,8 @@ mkdir -p ../OUTPUTS
 
 PCAP02=../OUTPUTS/basic-dao.pcap
 
+echo "set args --fake -v -d ../INPUTS/basic-dao.txt -O ${PCAP02} -i wlan0" >.gdbinit-02
+
 ${SENDDAO} --fake -v -d ../INPUTS/basic-dao.txt -O ${PCAP02} -i wlan0 2>&1 | tee ../OUTPUTS/senddao-test-02.raw | diff -B -w - senddao-test-02.out
 
 ${TCPDUMP-tcpdump} -t -n -r ${PCAP02} -v -v | tee ../OUTPUTS/senddao-test-02-pcap.txt | diff -B -w - senddao-test-02-pcap.txt
