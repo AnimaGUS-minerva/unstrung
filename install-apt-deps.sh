882473aaa92dfbb41f07e73cbc40f26c16ce3d4f
Michael Richardson <mcr@sandelman.ca>
2023-02-25 18:40:59 -0500

    update to list of debian packages required

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -4,7 +4,7 @@ if [ `uname -m` = "amd64" ] || [ `uname -m` = "i386" ]
 then
     sudo apt-get install gcc-multilib g++-multilibi lib32stdc++6 lib32z1-dev
 fi
-sudo apt-get install dpkg-dev pkg-config libssl1.0.0 libssl-dev
+sudo apt-get install dpkg-dev pkg-config libssl-dev
 #cp /usr/lib/x86_64-linux-gnu/libcrypto* /tmp
 
 sudo apt-get install build-essential libdbus-1-dev libusb-1.0-0-dev flex bison libboost-dev libcmocka-dev libcmocka0 \
