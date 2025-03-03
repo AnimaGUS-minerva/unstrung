f26a6b2e19d07782163c955fde6a0494993027fe
Michael Richardson <mcr@sandelman.ca>
2025-03-02 22:21:56 -0500

    when MBEDTLSLIB is not set, and USE_MBEDTLS is, then dynamically link libraries

--- include/mbedtls.h
+++ include/mbedtls.h
@@ -16,7 +16,7 @@ extern "C" {
 
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
-#include "mbedtls/net.h"
+#include "mbedtls/net_sockets.h"
 #include "mbedtls/error.h"
 #include "mbedtls/ssl.h"
 #include "mbedtls/x509.h"
